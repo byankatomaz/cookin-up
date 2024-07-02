18aecf7ade07e8c036f5765005c33a9617a99251
byankatomaz <byankatomaz@gmail.com>
2024-07-02 16:21:54 -0300

    feat: app new

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+import ConteudoPrincipal from "./components/ConteudoPrincipal.vue";
+import Rodape from "./components/Rodape.vue"
+
+export default {
+  components: { Banner, ConteudoPrincipal, Rodape }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+  <Rodape /> 
+</template>
