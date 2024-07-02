7f0b132b2bd24d92140bddec6027d00d1a7a3f69
byankatomaz <byankatomaz@gmail.com>
2024-07-02 16:22:54 -0300

    feat: file with principal button of application

---
+++ src/components/BotaoPrincipal.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {
+  props: {
+    texto: String,
+  },
+};
+</script>
+
+<template>
+  <button class="cor-botao">{{ texto }}</button>
+</template>
+
+<style scoped>
+.cor-botao {
+  color: var(--creme, #fffaf3);
+  background: var(--coral, #f0633c);
+  font-weight: 700;
+  padding: 1rem 6rem;
+  border-radius: 2rem;
+}
+</style>
