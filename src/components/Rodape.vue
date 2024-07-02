8c1098d638a0e22edd83b093c318b81d1d95ac84
byankatomaz <byankatomaz@gmail.com>
2024-07-02 16:25:31 -0300

    feat: file with footer of application

---
+++ src/components/Rodape.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <div class="background">
+    <h3>
+      Desenvolvido por Byanka | 2024 - Projeto fictício sem fins comerciais.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  height: 15rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  color: #fff;
+  background-color: #213627;
+  text-align: center;
+  box-sizing: border-box;
+}
+</style>
